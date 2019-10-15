a4b5729250712836bd041e5caa78cee6094f39eb
erwan-lcs <erwan@lancontrolsystems.com>
2019-10-15 08:53:15 +0100

    initial commit

---
+++ Mosaic Todo/Controllers/ToDoesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mosaic_Todo.Models;
+
+namespace Mosaic_Todo.Controllers
+{
+    public class ToDoesController : Controller
+    {
+        private ToDoDbContext db = new ToDoDbContext();
+
+        private IToDoRepositoryInterface _toDoRepo = new ToDoRepository();
+
+        public ActionResult Index()
+        {
+           
+            this.Session["ToDoes"] = _toDoRepo.GetAll();
+            ViewBag.ToDoes = _toDoRepo.GetAll();
+            return View(db.ToDoes.ToList());
+            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description,Completed")] ToDo toDo)
+        {
+
+            _toDoRepo.Add(toDo);
+            ViewBag.ToDoes = _toDoRepo.GetAll();
+            //return RedirectToAction("Index");
+
+            if (ModelState.IsValid)
+            {
+
+                db.ToDoes.Add(toDo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+        
+        // POST: ToDoes/Complete/5
+        [HttpPost, ActionName("Complete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Complete(int id, bool completed)
+        {
+            ToDo toDo = db.ToDoes.Find(id);
+
+            if (ModelState.IsValid)
+            {
+                toDo.Completed = completed;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            ToDo toDo = db.ToDoes.Find(id);
+            db.ToDoes.Remove(toDo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Mosaic Todo/Models/ToDo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Mosaic_Todo.Models;
+
+namespace Mosaic_Todo.Models
+{
+    public interface IToDoCompletable
+    {
+        void Complete();
+    }
+
+    public interface IToDoDeletable
+    {
+        void Delete();
+    }
+
+    public interface IToDoInterface : IToDoCompletable, IToDoDeletable
+    {
+        int Id { get; set; }
+        string Description { get; set; }
+        bool Completed { get; set; }
+    }
+
+    public class ToDo : IToDoInterface
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+
+        public void Complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ToDoDbContext : DbContext
+    {
+        public DbSet<ToDo> ToDoes { get; set; }
+    }
+
+    public interface IToDoRepositoryInterface
+    {
+        List<IToDoInterface> GetAll();
+        void Add(IToDoInterface toDo);
+        void Delete(int toDoId);
+        void Complete(int toDoId);
+    }
+
+    public class ToDoRepository : IToDoRepositoryInterface
+    {
+        private List<IToDoInterface> _toDoes;
+
+        public ToDoRepository()
+        {
+            this._toDoes = new List<IToDoInterface>();
+
+            ToDo defaultToDo = new ToDo();
+            defaultToDo.Id = 1;
+            defaultToDo.Description = "Default Task";
+            defaultToDo.Completed = false;
+            this._toDoes.Add(defaultToDo);
+        }
+
+        public List<IToDoInterface> GetAll()
+        {
+            return this._toDoes;
+        }
+
+        public void Add(IToDoInterface toDo)
+        {
+            toDo.Id = this._toDoes.Max(x => x.Id) + 1;
+            this._toDoes.Add(toDo);
+        }
+
+        public void Delete(int toDoId)
+        {
+            this._toDoes.Remove(this._toDoes.Where(x => x.Id == toDoId).First());
+            //Re-index
+            this._toDoes.ForEach((x) => { if (x.Id > toDoId) x.Id = x.Id - 1; });
+        }
+
+        public void Complete(int toDoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+
+
+}
